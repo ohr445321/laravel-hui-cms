84a4fa6a7871da2a2600c742f04e16530c56034a
wangDQ <1030026236@qq.com>
2017-03-01 18:06:17 +0800

    wdq commmit

--- resources/views/admin/role/role-permissions-iframe.blade.php
+++ resources/views/admin/role/role-permissions-iframe.blade.php
@@ -44,6 +44,7 @@
                 var self = this;
                 dom.post_url = "{{ url('admin/role/ajax-save-role-permissions') }}";
                 dom._token = "{{ csrf_token() }}";
+                dom.chkIds = [];
                 dom.setting = {
                     check: {
                         enable: true
@@ -58,70 +59,40 @@
                             idKey:"id",
                             pIdKey:"parent_id",
                             rootPId:null,
-                        }
+                        },
                     },
                     view: {
                         showIcon:false
                     },
                     callback:{
-                        beforeCheck:function(treeId,treeNode){
-                            console.log(treeId)
-                        },
                         onCheck:function(e,treeId,treeNode){
-                            console.log(treeNode)
+                            dom.chkIds = [];
+                            var zTree = $.fn.zTree.getZTreeObj("treeDemo");
+                            checkCount = zTree.getCheckedNodes(true).length;
+                            $.each(zTree.getCheckedNodes(true),function(index,item){
+                                dom.chkIds.push(item.id);
+                            })
                         }
                     }
                 };
                 $.post("{{ url('admin/role/ajax-get-role-permissions') }}", {'_token': this.dom._token, id: 1}, function (data) {
                     $.fn.zTree.init($("#treeDemo"), dom.setting, data.data);
-                    // var str = '<table id="tableTree">';
-                    // $.each(dom.data,function(index,item){
-                    //     var chk = '';
-                    //     if(item.is_select==1){
-                    //         chk='checked'
-                    //     }
-                    //     str += '<tr data-tt-id="'+item.id+'" data-tt-parent-id="'+item.parent_id+'"><td><input type="checkbox" name="role['+item.id+']" value="'+item.id+'" chk/>'+item.permissions_name+'</td></tr>'
-                    // })
-                    // str += "</table>";
-                    // // dom.data.each(function(index,item){
-                    // //     console.log(item);
-                    // // })
-                    // $('.tree').html(str);
-                    // $('#tableTree').treetable({
-                    //     expandable:true,
-                    //     indent:20,
-                    //     expanderTemplate:'<a href="#" class="Hui-iconfont Hui-iconfont-sanjiao"></a>'
-                    // });
-
                 }, 'json');
             },
+            relateChk:function(root){
+                console.log($(root).parents('tr'));
+            },
             vaildate:function(){
-                // var flag = true;
-                // var role_name = $("input[name='role_name']").val();
-
-                // if (!role_name || role_name == undefined) {
-                //     layer.msg('角色名称不能为空~', {icon: 5,time: 2500});
-                //     return false;
-                // }
-
-                // return flag;
             },
             pushData:function(){
                 var dom = this.dom;
-                var ids = [];
                 var role_id = $('input[name="role_id"]').val();
                 var data = {
                     role_id:role_id,
                     _token:dom._token,
                     permissions_ids : ''
                 }
-                $.each($('#treeDemo').find('.chk'),function(index,item){
-                    if($(item).hasClass('checkbox_true_part')||$(item).hasClass('checkbox_true_full')){
-                        var arr = $(item).attr('id').split('_');
-                        ids.push(arr[1]);
-                    }
-                })
-                data.permissions_ids = ids.join(',');
+                data.permissions_ids = dom.chkIds.join(',');
 
                 $.post(dom.post_url, data, function(msg){
                     if (msg.code == 0) {
@@ -135,7 +106,6 @@
             },
             bindEvent:function(){
                 var root = this;
-
                 $(document).on('click', '.submit-post', function(){
                     root.pushData();
                 });
